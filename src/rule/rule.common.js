393dadc5de86ff9e729ec05d97372302de54db43
chinakite <chinakite.zhang@gmail.com>
2017-04-06 07:46:22 +0800

    Refact rule parser.

--- src/rule/rule.common.js
+++ src/rule/rule.common.js
@@ -14,47 +14,53 @@ const objectRule = require('./rule.object');
  *
  *
  */
-function parseRule(rule) {
-	let ruleDesc = {};
+function parseDataTmpl(dataTmplDef) {
+	var tmplDesc = {};
+	for(var prop in dataTmplDef) {
+		var rule = dataTmplDef[prop];
+		
 
-	//rule = rule;
-	let ruleItemStrs = rule.split('|');
-	let dataType = ruleItemStrs[0];
 
-	if(supportedDataType.indexOf(dataType) < 0) {
-		console.log('Can not supported data type : ' + dataType);
-	}else{
-		ruleDesc.dataType = dataType;
-	}
+		let ruleDesc = {};
 
-	if(ruleItemStrs.length > 1) {
-		if(dataType == 'int') {
-			var intRuleDesc = intRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = intRuleDesc;
-		}else if(dataType == 'boolean' || dataType == 'bool') {
-			var boolRuleDesc = boolRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = boolRuleDesc;
-		}else if(dataType == 'string') {
-			var stringRuleDesc = stringRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = stringRuleDesc;
-		}else if(dataType == 'date') {
-			var dateRuleDesc = dateRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = dateRuleDesc;
-		}else if(dataType == 'datetime') {
-			var datetimeRuleDesc = datetimeRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = datetimeRuleDesc;
-		}else if(dataType == 'time') {
-			var timeRuleDesc = timeRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = timeRuleDesc;
-		}else if(dataType == 'float') {
-			var floatRuleDesc = floatRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = floatRuleDesc;
-		}else if(dataType == 'object') {
-			var objectRuleDesc = objectRule.parseRule(ruleItemStrs);
-			ruleDesc.desc = objectRuleDesc;
+		let ruleItemStrs = rule.split('|');
+		let dataType = ruleItemStrs[0];
+
+		if(supportedDataType.indexOf(dataType) < 0) {
+			console.log('Can not supported data type : ' + dataType);
+		}else{
+			ruleDesc.dataType = dataType;
+		}
+
+		if(ruleItemStrs.length > 1) {
+			if(dataType == 'int') {
+				var intRuleDesc = intRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = intRuleDesc;
+			}else if(dataType == 'boolean' || dataType == 'bool') {
+				var boolRuleDesc = boolRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = boolRuleDesc;
+			}else if(dataType == 'string') {
+				var stringRuleDesc = stringRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = stringRuleDesc;
+			}else if(dataType == 'date') {
+				var dateRuleDesc = dateRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = dateRuleDesc;
+			}else if(dataType == 'datetime') {
+				var datetimeRuleDesc = datetimeRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = datetimeRuleDesc;
+			}else if(dataType == 'time') {
+				var timeRuleDesc = timeRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = timeRuleDesc;
+			}else if(dataType == 'float') {
+				var floatRuleDesc = floatRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = floatRuleDesc;
+			}else if(dataType == 'object') {
+				var objectRuleDesc = objectRule.parseRule(ruleItemStrs);
+				ruleDesc.desc = objectRuleDesc;
+			}
 		}
 	}
-	return ruleDesc;
+	return tmplDesc;
 }
 
 module.exports = {
