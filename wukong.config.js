fda737076e363fd960c9d833358270192e4ef987
Chinakite Zhang <chinakite.zhang@gmail.com>
2018-06-19 17:09:21 +0800

    Update wukong.config.js
    change default engint to fs

--- wukong.config.js
+++ wukong.config.js
@@ -1,17 +1,17 @@
 //FileSystem Storage
 const config = {
     port : 7272,
-    // storage : {
-	// 	engine : 'fs',
-	// 	mappings  : ['data/mappings'],
-	// 	dataPath  : ['data/datas'],
-	// 	tmplPath  : ['data/tmpls'],
-    //     libPath   : ['data/lib'],
-    //     mylibPath : ['data/lib/my']
-	// },
     storage : {
-		engine : 'mysql'
-	},
+ 	engine : 'fs',
+ 	mappings  : ['data/mappings'],
+ 	dataPath  : ['data/datas'],
+ 	tmplPath  : ['data/tmpls'],
+        libPath   : ['data/lib'],
+        mylibPath : ['data/lib/my']
+    },
+    //storage : {
+    //		engine : 'mysql'
+    //	},
     log : {
         level : 'debug'
     },
